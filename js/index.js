501e5c4fb8758aeaf65e6d2dc6a10e1189b4b2cb
SeungEunChun <1000tmddms@gmail.com>
2024-02-08 12:54:06 +0900

    스와이퍼 한쪽방향으로 이동이 아니라 fade효과 나머지공식으로 구현

--- js/index.js
+++ js/index.js
@@ -10,13 +10,41 @@ document.querySelector("#popup button").addEventListener('click', function () {
 $(function () {//$(".swiper-wrapper").animate({ "marginLeft": "-="+800 }//끊임없이 옆으로 가게함(추가대입식)
 
 
-    function swiperfun() {
-        $(".swiper-wrapper").animate({ "marginLeft": -$(".swiper-slide").width() }, 500, function () {
-            $(".swiper-wrapper .swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
-            $(".swiper-wrapper").css("margin-left", 0)
-        })
-    }
-    setInterval(swiperfun, 3000)
+    // function swiperfun() {
+    //     $(".swiper-wrapper").animate({ "marginLeft": -$(".swiper-slide").width() }, 500, function () {
+    //         $(".swiper-wrapper .swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+    //         $(".swiper-wrapper").css("margin-left", 0)
+    //     })
+    // }
+    // setInterval(swiperfun, 3000)
+
+    // function swiperfade() {
+    //     count %= $('swiper-slide').length;
+
+    //     $(".swiper-slide").eq(count).addClass("active").siblings().removeClass("active");
+
+
+
+    // }
+    let count = 0;
+
+    setInterval(function () {
+        count++;
+        count %= $('.swiper-slide').length;
+
+        $(".swiper-slide").eq(count).addClass("active").siblings().removeClass("active");
+
+    }, 2000)
+
+
+
+
+
+
+
+
+
+
     // $("body").addClass("c")//jquery 손상여부 확인
     //$(".swiper-wrapper").animate({ "marginLeft": -$(".swiper-slide").width() }, 800, function () {
     //swiper-slide의 넓이만큼 marginLeft 애니메이션(jquery 짱)
